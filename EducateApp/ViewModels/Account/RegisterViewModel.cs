08224736351403b2b1f16e9625d0d217e5b2de03
Анастасия Чиняева <nastya.chinyaeva@gmail.com>
2021-10-26 20:41:53 +0500

    Добавление регистрации пользователя

---
+++ EducateApp/ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EducateApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Введите E-mail")]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Введите фамилию")]
+        [Display(Name = "Фамилия")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Введите имя")]
+        [Display(Name = "Имя")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Введите отчество")]
+        [Display(Name = "Отчество")]
+        public string Patronymic { get; set; }
+
+        [Required(ErrorMessage = "Введите пароль")]
+        [Display(Name = "Пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Введите повтор пароля")]
+        [Display(Name = "Подтвердите пароль")]
+        [Compare("Password", ErrorMessage = "Папроли не совпадают")]
+        [DataType(DataType.Password)]
+        public string PasswordConfirm { get; set; }
+    }
+}
