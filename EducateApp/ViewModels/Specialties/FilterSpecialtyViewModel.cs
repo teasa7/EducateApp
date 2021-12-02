5f55d64f339ed5d039da0c9ef8c58be5b8c86555
Евгений Куканов <3gt1@mail.ru>
2021-12-02 10:35:36 +0500

    Revert "Фильтрация дисциплины (не работающая)"
    This reverts commit 12d33a795a634b5ab98c91d76ed7f41072c13067.

--- EducateApp/ViewModels/Specialties/FilterSpecialtyViewModel.cs
+++ EducateApp/ViewModels/Specialties/FilterSpecialtyViewModel.cs
@@ -1,15 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using EducateApp.Models.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 
-namespace Texnicum.ViewModels.Specialties
+namespace EducateApp.ViewModels.Specialties
 {
     public class FilterSpecialtyViewModel
     {
-        public string SelectedCode { get; private set; }   
-        public string SelectedName { get; private set; }    
+        public string SelectedCode { get; private set; }  
+        public string SelectedName { get; private set; }   
 
-        public SelectList FormOfStudies { get; private set; } 
-        public short? FormOfEdu { get; private set; }   
+        public SelectList FormOfStudies { get; private set; }
+        public short? FormOfEdu { get; private set; }  
+
 
 
         public FilterSpecialtyViewModel(string code, string name,
@@ -18,7 +20,6 @@ namespace Texnicum.ViewModels.Specialties
             SelectedCode = code;
             SelectedName = name;
 
- 
             formOfStudies.Insert(0, new FormOfStudy { FormOfEdu = "", Id = 0 });
 
             FormOfStudies = new SelectList(formOfStudies, "Id", "FormOfEdu", formOfEdu);
